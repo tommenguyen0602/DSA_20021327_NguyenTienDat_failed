da84f6185378461b33b5fe9aed457ea45c3a4b8e
tommenguyen <tommenguyen0602@gmail.com>
2021-09-26 19:01:39 +0700

    faster

---
+++ LogicT.java
@@ -0,0 +1,6 @@
+public class LogicT {
+    public static void main(String[] args) {
+        int a = 3 / 2;
+        System.out.println(a);
+    }
+}
--- SherlockandArray.java
+++ SherlockandArray.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class SherlockandArray {
+    public static String check(int[] arr, int middle) {
+
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        while (T-- > 0) {
+            int N = scanner.nextInt();
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = scanner.nextInt();
+            }
+            System.out.println(check(arr,1));
+        }
+    }
 }
